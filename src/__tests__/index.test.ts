9f2b14ebba7957b5d66345e9754ae59994edb618
Amit Hemo <itsamit442@gmail.com>
2024-08-27 00:07:22 +0300

    update test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,3 @@
-import { sum } from '../index';
-
-describe('sum', () => {
-  it('should print the correct text from the file', async () => {
-    expect(sum(1, 2)).toEqual(3);
-  });
+describe('first test', () => {
+  it.todo('first test');
 });
