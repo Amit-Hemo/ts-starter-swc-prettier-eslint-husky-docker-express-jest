4a398dc1378442f265bdf73de81b4e6bea668464
Amit Hemo <itsamit442@gmail.com>
2024-08-26 22:27:42 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { readFile } from 'fs/promises';
-import path from 'path';
-console.log('hi');
-
-async function getText(path: string): Promise<string> {
-  const readText = await readFile(path, { encoding: 'utf-8' });
-  return readText;
+export function sum(x: number, y: number) {
+  return x + y;
 }
-
-const text = getText(path.join(__dirname, '..', 'hello.txt')).then((text) => console.log(text))
